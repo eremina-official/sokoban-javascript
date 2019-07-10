8504579e43f4b87a487b42bd2b7166b65d1f761a
Marina Eremina <eremina.jobmail@gmail.com>
2019-07-10 20:54:30 +0200

    move levels to a separate file

---
+++ levels.js
@@ -0,0 +1,22 @@
+const levels = [
+  [
+    'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall',
+
+    'wall', 'space', 'space', 'space', 'space','space', 'space', 'wall',
+
+    'wall', 'space', 'space', 'space', 'space', 'space', 'space', 'wall',
+
+    'wall', 'person', 'space', 'space', 'space', 'space', 'space', 'wall',
+
+    'wall', 'space', 'space', 'space', 'space', 'space', 'space', 'wall',
+
+    'wall', 'space', 'space', 'space', 'space', 'space', 'space', 'wall',
+
+    'wall', 'space', 'space', 'space', 'space', 'space', 'space', 'wall',
+
+    'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall'
+  ]
+];
+
+
+export default levels;
