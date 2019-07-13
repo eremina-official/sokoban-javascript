5e711762849fa6d580b4efd0f96dace398bf8163
Marina Eremina <eremina.jobmail@gmail.com>
2019-07-13 19:30:50 +0200

    add playNextGame function

--- src-oop/index.js
+++ src-oop/index.js
@@ -1,1 +1,25 @@
 import './game-oop.js';
+import levels from '../levels.js';
+import Game from './game-oop.js';
+
+
+let currentIndex = 0;
+const board = document.querySelector('.js-board');
+const winnerScreen = document.querySelector('.js-winner-screen');
+let newGame = new Game(levels[currentIndex]);
+
+document.addEventListener('click', playNextGame);
+
+function playNextGame(event) {
+  if (
+    event.target === document.querySelector('.js-winner-screen__button')
+    && currentIndex < levels.length - 1
+  ) {
+    currentIndex += 1;
+    board.textContent = '';
+    winnerScreen.classList.remove('is-visible');
+    newGame = new Game(levels[currentIndex]);
+    console.log(newGame);
+    console.log(currentIndex)
+  }
+}
