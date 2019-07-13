eab5e4651dc35cc2eba3a3b74f5bf749b875e684
Marina Eremina <eremina.jobmail@gmail.com>
2019-07-13 19:28:27 +0200

    update levels

--- levels.js
+++ levels.js
@@ -6,9 +6,27 @@ const levels = [
 
     'wall', 'space', 'space', 'space', 'space', 'space', 'space', 'wall',
 
-    'wall', 'person', 'space', 'space', 'space', 'space', 'space', 'wall',
+    'wall', 'person', 'space', 'space', 'box', 'target', 'space', 'wall',
+
+    'wall', 'space', 'space', 'space', 'box', 'target', 'space', 'wall',
 
     'wall', 'space', 'space', 'space', 'space', 'space', 'space', 'wall',
+
+    'wall', 'space', 'space', 'space', 'space', 'space', 'space', 'wall',
+
+    'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall'
+  ],
+
+  [
+    'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall',
+
+    'wall', 'person', 'space', 'space', 'space','space', 'space', 'wall',
+
+    'wall', 'space', 'space', 'space', 'space', 'space', 'space', 'wall',
+
+    'wall', 'space', 'space', 'space', 'space', 'space', 'space', 'wall',
+
+    'wall', 'space', 'box', 'target', 'space', 'space', 'space', 'wall',
 
     'wall', 'space', 'space', 'space', 'space', 'space', 'space', 'wall',
 
