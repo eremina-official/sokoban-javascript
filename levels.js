8c27e2cd4577c415aaec543ed5350c96e0ca52c4
Marina Eremina <eremina.jobmail@gmail.com>
2019-07-18 22:22:02 +0200

    change levels structure

--- levels.js
+++ levels.js
@@ -1,31 +1,25 @@
 const levels = [
   [
-    [
-      'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall',
-      'wall', 'space', 'space', 'space', 'space','space', 'space', 'wall',
-      'wall', 'space', 'space', 'space', 'space', 'space', 'space', 'wall',
-      'wall', 'person', 'space', 'space', 'box', 'space', 'space', 'wall',
-      'wall', 'space', 'space', 'space', 'box', 'space', 'space', 'wall',
-      'wall', 'space', 'space', 'space', 'space', 'space', 'space', 'wall',
-      'wall', 'space', 'space', 'space', 'space', 'space', 'space', 'wall',
-      'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall'
-    ],
-    [29, 37] //indices of targets
+    'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall',
+    'wall', 'space', 'space', 'space', 'space','space', 'space', 'wall',
+    'wall', 'space', 'space', 'space', 'space', 'space', 'space', 'wall',
+    'wall', 'person', 'space', 'space', 'box', 'target', 'space', 'wall',
+    'wall', 'space', 'space', 'space', 'box', 'target', 'space', 'wall',
+    'wall', 'space', 'space', 'space', 'space', 'space', 'space', 'wall',
+    'wall', 'space', 'space', 'space', 'space', 'space', 'space', 'wall',
+    'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall'
   ],
 
   [
-    [
-      'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall',
-      'wall', 'person', 'space', 'space', 'space','space', 'space', 'wall',
-      'wall', 'space', 'space', 'space', 'space', 'space', 'space', 'wall',
-      'wall', 'space', 'space', 'space', 'space', 'space', 'space', 'wall',
-      'wall', 'space', 'box', 'space', 'space', 'space', 'space', 'wall',
-      'wall', 'space', 'space', 'space', 'space', 'space', 'space', 'wall',
-      'wall', 'space', 'space', 'space', 'space', 'space', 'space', 'wall',
-      'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall'
-    ],
-    [35]
-  ]
+    'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall',
+    'wall', 'person', 'space', 'space', 'space','space', 'space', 'wall',
+    'wall', 'space', 'space', 'space', 'space', 'space', 'space', 'wall',
+    'wall', 'space', 'space', 'space', 'space', 'space', 'space', 'wall',
+    'wall', 'space', 'box', 'space', 'target', 'space', 'space', 'wall',
+    'wall', 'space', 'space', 'space', 'space', 'space', 'space', 'wall',
+    'wall', 'space', 'space', 'space', 'space', 'space', 'space', 'wall',
+    'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall'
+  ],
 ];
 
 
