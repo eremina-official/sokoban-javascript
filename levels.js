81ae6231bdda751005bbcb0f4da8e3db95f9b21f
Marina Eremina <eremina.jobmail@gmail.com>
2019-07-19 19:48:52 +0200

    edit levels

--- levels.js
+++ levels.js
@@ -1,25 +1,29 @@
 const levels = [
   [
-    'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall',
-    'wall', 'space', 'space', 'space', 'space','space', 'space', 'wall',
+    'wall', 'wall',   'wall',  'wall',  'wall',  'wall',   'wall',  'wall',
+    'wall', 'space',  'space', 'space', 'space', 'space',  'space', 'wall',
+    'wall', 'person',  'space', 'space', 'space', 'space',  'space', 'wall',
     'wall', 'space', 'space', 'space', 'space', 'space', 'space', 'wall',
-    'wall', 'person', 'space', 'space', 'box', 'target', 'space', 'wall',
-    'wall', 'space', 'space', 'space', 'box', 'target', 'space', 'wall',
-    'wall', 'space', 'space', 'space', 'space', 'space', 'space', 'wall',
-    'wall', 'space', 'space', 'space', 'space', 'space', 'space', 'wall',
-    'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall'
+    'wall', 'space',  'space', 'box', 'space',   'target', 'space', 'wall',
+    'wall', 'space',  'space', 'space', 'space', 'space',  'space', 'wall',
+    'wall', 'space',  'space', 'space', 'space', 'space',  'space', 'wall',
+    'wall', 'wall',   'wall',  'wall',  'wall',  'wall',   'wall',  'wall'
   ],
 
   [
-    'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall',
-    'wall', 'person', 'space', 'space', 'space','space', 'space', 'wall',
-    'wall', 'space', 'space', 'space', 'space', 'space', 'space', 'wall',
-    'wall', 'space', 'space', 'space', 'space', 'space', 'space', 'wall',
+    'space', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall',
+    'wall', 'wall', 'space', 'space', 'space','space', 'space', 'wall',
+    'wall', 'space', 'person', 'space', 'space', 'space', 'space', 'wall',
+    'wall', 'space', 'space', 'space', 'wall', 'space', 'space', 'wall',
     'wall', 'space', 'box', 'space', 'target', 'space', 'space', 'wall',
     'wall', 'space', 'space', 'space', 'space', 'space', 'space', 'wall',
     'wall', 'space', 'space', 'space', 'space', 'space', 'space', 'wall',
     'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall'
   ],
+
+  [],
+
+  []
 ];
 
 
