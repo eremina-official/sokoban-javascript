fc7f4c8de2740a65918f267409a715e680f4708c
Marina Eremina <eremina.jobmail@gmail.com>
2019-07-20 19:58:33 +0200

    change levels implementaion, add keys for levels and mapping function

--- levels.js
+++ levels.js
@@ -1,30 +1,81 @@
-const levels = [
+const rawLevels = [
+  //1
   [
-    'wall', 'wall',   'wall',  'wall',  'wall',  'wall',   'wall',  'wall',
-    'wall', 'space',  'space', 'space', 'space', 'space',  'space', 'wall',
-    'wall', 'person',  'space', 'space', 'space', 'space',  'space', 'wall',
-    'wall', 'space', 'space', 'space', 'space', 'space', 'space', 'wall',
-    'wall', 'space',  'space', 'box', 'space',   'target', 'space', 'wall',
-    'wall', 'space',  'space', 'space', 'space', 'space',  'space', 'wall',
-    'wall', 'space',  'space', 'space', 'space', 'space',  'space', 'wall',
-    'wall', 'wall',   'wall',  'wall',  'wall',  'wall',   'wall',  'wall'
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 1, 1, 1, 1, 1, 1, 0,
+    0, 2, 1, 1, 1, 1, 1, 0,
+    0, 1, 1, 1, 1, 1, 1, 0,
+    0, 1, 1, 3, 1, 4, 1, 0,
+    0, 1, 1, 1, 1, 1, 1, 0,
+    0, 1, 1, 1, 1, 1, 1, 0,
+    0, 0, 0, 0, 0, 0, 0, 0
   ],
-
+  //2
   [
-    'space', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall',
-    'wall', 'wall', 'space', 'space', 'space','space', 'space', 'wall',
-    'wall', 'space', 'person', 'space', 'space', 'space', 'space', 'wall',
-    'wall', 'space', 'space', 'space', 'wall', 'space', 'space', 'wall',
-    'wall', 'space', 'box', 'space', 'target', 'space', 'space', 'wall',
-    'wall', 'space', 'space', 'space', 'space', 'space', 'space', 'wall',
-    'wall', 'space', 'space', 'space', 'space', 'space', 'space', 'wall',
-    'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall', 'wall'
+    1, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 1, 1, 1, 1, 1, 0,
+    0, 1, 1, 1, 1, 1, 1, 0,
+    0, 1, 1, 1, 0, 0, 4, 0,
+    0, 1, 2, 1, 1, 1, 1, 0,
+    0, 1, 3, 0, 0, 1, 1, 0,
+    0, 1, 1, 1, 1, 1, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 1
   ],
-
-  [],
-
+  //3
+  [
+    0, 0, 0, 0, 0, 1, 1, 1,
+    0, 1, 4, 1, 0, 0, 1, 1,
+    0, 0, 1, 1, 1, 0, 0, 1,
+    1, 0, 1, 1, 1, 1, 0, 0,
+    1, 0, 0, 1, 1, 3, 1, 0,
+    1, 1, 0, 0, 1, 1, 1, 0,
+    1, 1, 1, 0, 1, 2, 1, 0,
+    1, 1, 1, 0, 0, 0, 0, 0
+  ],
+  //4
+  [
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 4, 1, 1, 1, 1, 1, 0,
+    0, 1, 0, 1, 0, 0, 1, 0,
+    0, 1, 1, 1, 1, 1, 1, 0,
+    0, 1, 0, 1, 0, 1, 1, 0,
+    0, 1, 0, 1, 1, 3, 1, 0,
+    0, 1, 1, 1, 1, 2, 1, 0,
+    0, 0, 0, 0, 0, 0, 0, 0
+  ],
+  //5
   []
 ];
+
+const keys = {
+  0: 'wall',
+  1: 'space',
+  2: 'person',
+  3: 'box',
+  4: 'target'
+};
+
+const levels = rawLevels.map(level => {
+  return level.map(item => {
+    switch (item) {
+      case 0:
+        return item = keys[0];
+        break;
+      case 1:
+        return item = keys[1];
+        break;
+      case 2:
+        return item = keys[2];
+        break;
+      case 3:
+        return item = keys[3];
+        break;
+      case 4:
+        return item = keys[4];
+        break;
+    }
+  });
+});
 
 
 export default levels;
