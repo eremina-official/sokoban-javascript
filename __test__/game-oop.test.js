2927a6417cf539afd1b88ea905180cf5dc8fa6f5
Marina Eremina <eremina.jobmail@gmail.com>
2019-08-27 14:15:28 +0200

    add unit tests

--- __test__/game-oop.test.js
+++ __test__/game-oop.test.js
@@ -36,3 +36,45 @@ describe('getTargets', () => {
     expect(result).toEqual(expectedResult);
   });
 });
+
+
+describe('getPersonPosition', () => {
+  test('should return person position', () => {
+    const level = [
+      ['wall', 'space', 'space', 'target', 'space', 'box'],
+      ['wall', 'space', 'space', 'person', 'wall'],
+      ['wall', 'space', 'space', 'space', '', 'wall'],
+      ['wall', 'box', 'target', 'target', 'wall']
+    ];
+
+    const expectedResult = {personY: 1, personX: 3};
+
+    const result = Game.prototype.getPersonPosition(level);
+    expect(result).toEqual(expectedResult);
+  });
+});
+
+
+describe('getDeepCopy', () => {
+  test('should return a deep copy of an array', () => {
+    const level = [
+      ['wall', 'space', 'space', 'target', 'space', 'box'],
+      ['wall', 'space', 'space', 'person', 'wall'],
+      ['wall', 'space', 'space', 'space', 'space', 'wall'],
+      ['wall', 'box', 'target', 'target', 'wall']
+    ];
+    const exp = level;
+    const expectedResult = [
+      ['wall', 'space', 'space', 'target', 'space', 'box'],
+      ['wall', 'space', 'space', 'person', 'wall'],
+      ['wall', 'space', 'space', 'space', 'space', 'wall'],
+      ['wall', 'box', 'target', 'target', 'wall']
+    ];
+
+    const result = Game.prototype.getDeepCopy(level);
+    expect(result).toStrictEqual(expectedResult);
+    expect(result).toEqual(expectedResult);
+    console.log(level[0] === exp[0]);
+    console.log(level[0] === result[0])
+  });
+});
