69e5c4416098633d7ef59725e09d93063fd9dc44
Marina Eremina <eremina.jobmail@gmail.com>
2019-10-28 09:14:56 +0100

    add LevelMakerView class which contains view logic for LevelMaker

---
+++ src-oop/js/level-maker-view.js
@@ -0,0 +1,70 @@
+/**
+ * Renders the view according to a model.
+ * 
+ * @property {object} boardRowSelectElement - select DOM element
+ * @property {object} boardColumnSelectElement - select DOM element
+ * @property {object} selectOptionsRange - range of board rows and columns amount
+ * @property {object} levelMakerBoardElement - board DOM element
+ */
+class LevelMakerView {
+  constructor() {
+    this.boardRowSelectElement = document.querySelector('#board-row');
+    this.boardColumnSelectElement = document.querySelector('#board-column');
+    this.selectOptionsRange = {bottom: 6, top: 18};
+    this.levelMakerBoardElement = document.querySelector('.js-level-maker-board');
+    this.renderSelectOptions(this.selectOptionsRange, this.boardRowSelectElement);
+    this.renderSelectOptions(this.selectOptionsRange, this.boardColumnSelectElement);
+  }
+
+  /**
+   * Renders options for select elements.
+   * 
+   * @param {object} - range of options
+   * @param {object} - select DOM element
+   */
+  renderSelectOptions(range, DOMelement) {
+    const fragment = document.createDocumentFragment();
+    let i = range.bottom;
+    while (i <= range.top) {
+      const option = document.createElement('option');
+      option.setAttribute('value', i);
+      option.textContent = i;
+      fragment.appendChild(option);
+      i++;
+    }
+    fragment.firstChild.setAttribute('selected', true);
+    DOMelement.appendChild(fragment);
+  }
+
+  /**
+   * Renders board to the DOM.
+   * 
+   * @param {object} levelArray - array representing sokoban board
+   */
+  renderLevelMakerBoard(levelArray) {
+    const fragment = document.createDocumentFragment();
+  
+    levelArray.forEach(row => {
+      const rowElement = document.createElement('div');
+      rowElement.setAttribute('class', 'board__row js-board-row');
+  
+      row.forEach(item => {
+        const squareElement = document.createElement('div');
+        squareElement.setAttribute('class', `square js-level-maker-square`);
+        squareElement.setAttribute('data-square', `${item}`)
+        rowElement.appendChild(squareElement);
+      });
+  
+      fragment.appendChild(rowElement);
+    });
+  
+    this.levelMakerBoardElement.appendChild(fragment);
+  }
+
+  clearElementContent(DOMelement) {
+    DOMelement.textContent = '';
+  }
+}
+
+
+export default LevelMakerView;
--- src-oop/js/level-maker.js
+++ src-oop/js/level-maker.js
@@ -1,4 +1,5 @@
 import LevelMakerPlay from './level-maker-play.js';
+import LevelMakerView from './level-maker-view.js';
 
 
 /**
@@ -6,11 +7,10 @@ import LevelMakerPlay from './level-maker-play.js';
  * 
  * @property {object} boardRowSelectElement - select DOM element
  * @property {object} boardColumnSelectElement - select DOM element
+ * @property {LevelMakerView} levelMakerView - a LevelMakerView instance 
  * @property {object} boardSizeSubmitButtonElement - button DOM element
- * @property {object} levelMakerBoardElement - board DOM element
  * @property {object} playContainerElement - play container DOM element
  * @property {object} notificationElement - notification DOM element
- * @property {object} selectOptionsRange - range of board rows and columns amount
  * @property {string} squareInputCheckedValue - value of currently selected radio input with name="board-square-input"
  */
 
@@ -18,19 +18,16 @@ class LevelMaker {
   constructor() {
     this.boardRowSelectElement = document.querySelector('#board-row');
     this.boardColumnSelectElement = document.querySelector('#board-column');
+    this.levelMakerView = new LevelMakerView();
     this.boardSizeSubmitButtonElement = document.querySelector('.js-board-size-submit');
-    this.levelMakerBoardElement = document.querySelector('.js-level-maker-board');
     this.playContainerElement = document.querySelector('.js-play-container');
     this.notificationElement = document.querySelector('.js-notification');
-    this.selectOptionsRange = {bottom: 6, top: 18};
     this.squareInputCheckedValue = 'wall';
     this.handleBoardSize = this.handleBoardSize.bind(this);
     this.handleSquareInputChange = this.handleSquareInputChange.bind(this);
     this.handleSquareChange = this.handleSquareChange.bind(this);
     this.playCurrentLevelMaker = this.playCurrentLevelMaker.bind(this);
     this.bindEvents();
-    this.renderSelectOptions(this.selectOptionsRange, this.boardRowSelectElement);
-    this.renderSelectOptions(this.selectOptionsRange, this.boardColumnSelectElement);
   }
 
   bindEvents() {
@@ -38,26 +35,6 @@ class LevelMaker {
     document.addEventListener('change', this.handleSquareInputChange);
     document.addEventListener('click', this.handleSquareChange);
     document.addEventListener('click', this.playCurrentLevelMaker);
-  }
-
-  /**
-   * Renders options for select elements.
-   * 
-   * @param {object} - range of options
-   * @param {object} - select DOM element
-   */
-  renderSelectOptions(range, DOMelement) {
-    const fragment = document.createDocumentFragment();
-    let i = range.bottom;
-    while (i <= range.top) {
-      const option = document.createElement('option');
-      option.setAttribute('value', i);
-      option.textContent = i;
-      fragment.appendChild(option);
-      i++;
-    }
-    fragment.firstChild.setAttribute('selected', true);
-    DOMelement.appendChild(fragment);
   }
 
   /**
@@ -104,37 +81,8 @@ class LevelMaker {
    * @param {object} levelArray - array representing sokoban board
    */
   makeNewLevelMakerBoard(levelArray) {
-    this.clearElementContent(this.levelMakerBoardElement);
-    this.renderLevelMakerBoard(levelArray);
-  }
-
-  clearElementContent(DOMelement) {
-    DOMelement.textContent = '';
-  }
-
-  /**
-   * Renders board to the DOM.
-   * 
-   * @param {object} levelArray - array representing sokoban board
-   */
-  renderLevelMakerBoard(levelArray) {
-    const fragment = document.createDocumentFragment();
-  
-    levelArray.forEach(row => {
-      const rowElement = document.createElement('div');
-      rowElement.setAttribute('class', 'board__row js-board-row');
-  
-      row.forEach(item => {
-        const squareElement = document.createElement('div');
-        squareElement.setAttribute('class', `square js-level-maker-square`);
-        squareElement.setAttribute('data-square', `${item}`)
-        rowElement.appendChild(squareElement);
-      });
-  
-      fragment.appendChild(rowElement);
-    });
-  
-    this.levelMakerBoardElement.appendChild(fragment);
+    this.levelMakerView.clearElementContent(this.levelMakerBoardElement);
+    this.levelMakerView.renderLevelMakerBoard(levelArray);
   }
 
   /**
